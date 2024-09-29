3d9d1938acece6f1aec986885cecb55a92ad6ca8
kiki <lioukiki@gmail.com>
2024-09-29 20:40:47 +0800

    hw2

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
 
-@app.get("/")
+# http://localhost:8080/
+@app.get("/", status_code=200)
 async def root():
     return {"message": "Hello World"}
+
+
+@app.put("/items/{item_id}", status_code=200)
+async def update_item(item_id: int, item: dict, q: str = Query(None)):
+    response_data = {
+        "item_id": item_id,
+        "name": item["name"],
+        "description": item["description"],
+        "price": item["price"],
+        "tax": item["tax"],
+    }
+    
+    if q:
+        response_data["q"] = q 
+
+    return response_data
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
